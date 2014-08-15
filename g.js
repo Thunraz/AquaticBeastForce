5517e93be46e8a24cacfd5e5935482be4dd6c377
Thunraz <julian.dinges@gmail.com>
2014-08-15 22:36:54 +0200

    Added support for shooting

--- g.js
+++ g.js
@@ -27,8 +27,8 @@ GameLoop.prototype.frame = function(time) {
 */
 
 function Controls() {
-    this.codes  = { 37: 'left', 39: 'right', 38: 'up', 40: 'down' };
-    this.states = { 'left': false, 'right': false, 'up': false, 'down': false };
+    this.codes  = { 37: 'left', 39: 'right', 38: 'up', 40: 'down', 32: 'shoot' };
+    this.states = { 'left': false, 'right': false, 'up': false, 'down': false, 'shoot': false };
     
     document.addEventListener('keydown', this.onKey.bind(this, true), false);
     document.addEventListener('keyup', this.onKey.bind(this, false), false);
