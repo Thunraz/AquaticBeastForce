9d64696f15230c0b967b9ccb80e72b13e97ca259
Thunraz <julian.dinges@gmail.com>
2014-09-12 19:12:36 +0200

    Improved gulpfile a bit... maybe. Still has to be run twice to get the g.js... :(

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,33 @@ var gulp = require('gulp'),
     zip = require('gulp-zip'),
     htmlmin = require('gulp-htmlmin');
 
-gulp.task('default', function() {
-    gulp.src('lib/*.js')
+gulp.task('minify', function() {
+    gulp.src([
+            'lib/bitmap.js',
+            'lib/camera.js',
+            'lib/controls.js',
+            'lib/explosion.js',
+            'lib/gameLoop.js',
+            'lib/player.js',
+            'lib/shadow.js',
+            'lib/projectile.js',
+            'lib/groundTile.js',
+            'lib/enemy.js',
+            'lib/main.js'
+        ])
         .pipe(concat('g.js'))
         .pipe(uglify())
         .pipe(gulp.dest('.'));
+});
 
-    gulp.src(['g.js', 'index.html', 'assets/*'], { base: './' })
+gulp.task('zip', function() {
+    gulp.src(['g.js', 'index.html', 'assets/*.png'], { base: './' })
         .pipe(zip('AquaticBeastForce.zip'))
         .pipe(filesize())
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function() {
+    gulp.start('minify');
+    gulp.start('zip');
 });
